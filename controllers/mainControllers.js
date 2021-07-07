92831a9823f1db7b0b30e0dbcff2dbd67c73aaa8
DevHumbleChris <dev.humblechris254@gmail.com>
2021-07-07 16:08:07 +0300

    Commit Wed Jul 7 (@4:08pm)

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,8 +1,18 @@
-const MessagingResponse = require("twilio").twiml.MessagingResponse
-const response = new MessagingResponse()
+// const MessagingResponse = require("twilio").twiml.MessagingResponse
+// const response = new MessagingResponse()
+
+require("dotenv").config()
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 module.exports = {
   getUserRequest: (req, res) => {
-    response.message("Hello There")
+    client.messages
+          .create({
+             body: `This Message Is Sent From The Console ${req.body.MessageSid} Love`,
+             from: 'whatsapp:+14155238886',
+             to: 'whatsapp:+254768879348'
+           })
+          .then(message => console.log(message.sid))
+          .done();
   }
 }
