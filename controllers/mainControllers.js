cfa5f5d1b5e931a39b3fa35191cfab8d3fed1811
DevHumbleChris <dev.humblechris254@gmail.com>
2021-07-07 17:40:34 +0300

    Commit Wed Jul 7 (@5:40pm)

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,23 +1,28 @@
-const MessagingResponse = require("twilio").twiml.MessagingResponse
-const response = new MessagingResponse()
-
 require("dotenv").config()
 const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 module.exports = {
-  getUserRequest: (req, res) => {
-    // console.log(process.env.accountSid)
-    const message = response.message();
-    message.body('Hello World!');
-    res.redirect('/');
-    // client.messages
-    //       .create({
-    //          body: req.body.message,
-    //          from: 'whatsapp:+14155238886',
-    //          to: 'whatsapp:+254768879348'
-    //        })
-    //       .then(message => console.log(message.sid))
-    //       .done();
-          // res.render('index')
+  getUserRequest: async (req, res) => {
+    const userResponse = ['hi', 'hello', 'info']
+    isUserResponse = userResponse.includes(req.body.Body.toLowerCase())
+    if(isUserResponse) {
+      try {
+        await client.messages.create({
+          body: `Hello there ${req.body.ProfileName}, am *BotMontana*.Your Favourite WhatsAppBot here to help you, be able search movies, get movie/series updates that are trending and so much more.\n
+Type *trending* to get all trending movies/series.
+Type *trending movies* to get all trending movies.
+Type *trending series* to get all trending series.
+Type *search movie_name* to get your movie search results.`,
+          to: req.body.From,
+          from: req.body.To
+        }).done()
+      }catch(err) {
+        await client.messages.create({
+          body: `Someting wrong has happen: " ${err.message}""`,
+          to: req.body.From,
+          from: req.body.To
+        }).done()
+      }
+    }
   }
 }
