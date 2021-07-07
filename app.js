d705462c1dd544b2b19896f7489dcfc24dc75a5a
DevHumbleChris <dev.humblechris254@gmail.com>
2021-07-07 15:34:48 +0300

    Commit Wed Jul 7 (@3:34pm)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path")
 const mainRoutes = require("./routes/mainRoutes")
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 // Middlewares.
 app.use(morgan("tiny"))
