b50b434314aa8f32706299839eae8ab20cade02f
Am.Chris_KE <63234437+DevHumbleChris@users.noreply.github.com>
2021-08-03 19:46:39 +0300

    updated mainRoute

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,12 +1,6 @@
 const route = require("express").Router()
 const mainControllers = require("../controllers/mainControllers")
 
-route.get("/", (req, res) => {
-  res.render("index", {
-    message: 'Am Back!'
-  })
-})
-
 route.post("/", mainControllers.getUserRequest)
 
 module.exports = route
